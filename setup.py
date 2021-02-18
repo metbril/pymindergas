83d71c21ba54dee2a1bfbef9a68cf912e20cd44d
Robert van Bregt <1233531-metbril@users.noreply.gitlab.com>
2021-02-18 21:56:53 +0100

    fix: Update version for CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pymindergas", # Replace with your own username
-    version="0.1.0",
+    version="0.1.2",
     author="Robert van Bregt",
     author_email="robertvanbregt@gmail.com",
     description="A python module to post meter readings to Mindergas",
